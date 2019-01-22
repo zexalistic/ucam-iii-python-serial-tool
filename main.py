bfa52f08f7a62e2df07d7b288ad8ccbddae2ad00
Yihao Liu <YIL194@pitt.edu>
2019-01-22 14:03:05 -0500

    a few corrections so that it could work on my python3 machine; also fixed some bugs

--- main.py
+++ main.py
@@ -5,16 +5,15 @@ import sys
 
 cam = ucam.UCam()
 
-# cam.reset()
-# print("send reset")
-# time.sleep(5)
+#cam.reset()
+#print("send reset")
+#time.sleep(5)
 
 synced = cam.sync()
 print("synced is ready? {}".format(synced))
 
 if synced:
     # after synced, have to wait 1 to 2 seconds
-    time.sleep(2)
     filename = 'test.jpg'
     if len(sys.argv) > 1:
         filename = sys.argv[1]
--- ucam.py
+++ ucam.py
@@ -7,6 +7,9 @@ from binascii import hexlify, unhexlify
 from struct import pack, unpack
 import math
 import codecs
+from PIL import Image
+import io
+import numpy as np
 
 class UCam(object):
     """
@@ -16,9 +19,9 @@ class UCam(object):
 
     def __init__(self):
         # change serial device name to yours 
-        self.ser = serial.Serial('/dev/tty.usbserial-xxxxxxxxx', baudrate=921600, timeout=.01)
+        self.ser = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=1)
         self.synced = False
-        print("initialized!")
+        print("Serial port set")
 
     def sync(self):
         num_tries = 60      # 60 times is enough by the documents
@@ -29,8 +32,6 @@ class UCam(object):
         return False
 
     def _write(self, string):
-
-        
         return self.ser.write(bytearray(unhexlify(string)))
 
     
@@ -50,11 +51,8 @@ class UCam(object):
         return False
 
     def _initial(self):
-        init_cmd = commands.initial('07', '07', '07')
+        init_cmd = commands.initial('03', '01', '03')
         print("init cmd {}".format(init_cmd))
-
-
-
 
         self._write(init_cmd)
         # print("send init commandだよ")
@@ -63,8 +61,8 @@ class UCam(object):
 
         read = self._wait_for_bytes(6)
 
-        print("return ack, maybe AA 0E 01 xx 00 00")
-        print('ack {}'.format(read))
+        print("return ack, should be AA 0E 01 xx 00 00")
+        print('ack {}'.format(hexlify(read)))
 
 
         assert self._matches(commands.ack('01', '..'), read)
@@ -85,15 +83,20 @@ class UCam(object):
         assert self._matches(commands.ack('06', '..'), self._wait_for_bytes(6))
 
     def _snapshot(self):
-        self._write(commands.snapshot('00', '00', '00'))
-        assert self._matches(commands.ack('05', '..'), self._wait_for_bytes(6))
+        self._write(commands.snapshot('01', '00', '00'))
+        read = self._wait_for_bytes(6)
+        print("ack = {}".format(hexlify(read)))
+        assert self._matches(commands.ack('05', '..'), read)
+        #assert self._matches(commands.ack('05', '..'), self._wait_for_bytes(6))
 
     def _get_picture(self):
         """
         sends the GET PICTURE command and receives the corresponding DATA command.
         Returns the number of packets to be read.
         """
+        time.sleep(1)
         self._write(commands.get_picture('01'))
+        time.sleep(1)
         assert self._matches(commands.ack('04', '..'), self._wait_for_bytes(6))
         # receive DATA
         data = self._wait_for_bytes(6)
@@ -101,9 +104,6 @@ class UCam(object):
         assert self._matches(commands.data('01', '..', '..', '..'), data)
 
         print("hexlify(data) is: ", hexlify(data))
-
-        print("data is", data)
-        print("data[-3:] is ", data[-3:])
 
         # below line is too redundant... to avoid "UnicodeDecodeError: 'utf-8' codec can't decode byte 0x9a in position 0: invalid start byte"
         # img_size = unpack('<I', (codecs.decode(codecs.encode(unhexlify(hexlify(data[-3:])), 'hex'), 'hex') + b'\x00'))[0]
@@ -122,41 +122,31 @@ class UCam(object):
 
         print("num packages: {}".format(num_pkgs))
 
+        time.sleep(1)
         self._write(commands.ack('00', '00'))
+        time.sleep(1)
+
         return img_size
 
     def _write_picture(self, img_size, name='pic.jpeg'):
 
-        ### num_pkgs must be int
-        # num_pkgs = img_size / (512 - 6)
-        num_pkgs = math.floor(img_size / (512 - 6))
+        num_pkgs = 1
 
-
-        with open(name, 'wb+') as f:
-            for i in range(1, num_pkgs + 1):
-                # print("getting package {}".format(i))
-                read = self._wait_for_bytes(512)
-                # print(read)
-                f.write(read[4:-2])
-
-                ### hex_idx must be str
-                # hex_idx = hexlify(pack('H', i))
-                hex_idx = hexlify(pack('H', i)).decode()
-
-                # print("hex_idx is {}".format(hex_idx))
-                self._write(commands.ack('00', '00', hex_idx[:2], hex_idx[-2:]))
-            f.write(self._wait_for_bytes(img_size - num_pkgs * (512 - 6) + 2))
-            f.close()
+        read = self._wait_for_bytes(4800)
+        print(hexlify(read))
+        gray = np.array(read).reshape(60,80)
+        img = Image.fromarray(gray)
+        img.save('test.png')
         # ACK end of data transfer
-        self._write(commands.ack('f0', 'f0'))
+        self._write(commands.ack('0A', '00','01','00'))
         print("taken picture, finish!")
 
     def take_picture(self, name='pic.jpeg'):
-        # initialize for JPEG, VGA
+        # initialize for GREY, RAW
         self._initial()
         
         # set package size to 512 bytes
-        self._set_pkg_size()
+#        self._set_pkg_size()
 
         # compresed snapshot pic
         self._snapshot()
